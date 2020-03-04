3cc32bbffbaf311167b212c7f52ef0edf6bc4856
boranb <berkayboran@gmail.com>
2020-03-04 21:24:58 +0300

    Add project files.

---
+++ OzIMDB/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace OzIMDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnYeniFilm = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtFilmId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gboPuan = new System.Windows.Forms.GroupBox();
+            this.rbPuan5 = new System.Windows.Forms.RadioButton();
+            this.rbPuan4 = new System.Windows.Forms.RadioButton();
+            this.rbPuan3 = new System.Windows.Forms.RadioButton();
+            this.rbPuan2 = new System.Windows.Forms.RadioButton();
+            this.rbPuan1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFilmAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pboFoto = new System.Windows.Forms.PictureBox();
+            this.lstFilmler = new System.Windows.Forms.ListBox();
+            this.rbPuanYok = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.gboPuan.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnYeniFilm
+            // 
+            this.btnYeniFilm.Location = new System.Drawing.Point(9, 304);
+            this.btnYeniFilm.Name = "btnYeniFilm";
+            this.btnYeniFilm.Size = new System.Drawing.Size(200, 23);
+            this.btnYeniFilm.TabIndex = 6;
+            this.btnYeniFilm.Text = "Yeni Film Ekle";
+            this.btnYeniFilm.UseVisualStyleBackColor = true;
+            this.btnYeniFilm.Click += new System.EventHandler(this.btnYeniFilm_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtFilmId);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnYeniFilm);
+            this.groupBox1.Controls.Add(this.gboPuan);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtFilmAd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pboFoto);
+            this.groupBox1.Location = new System.Drawing.Point(10, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(389, 350);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Film Detayları";
+            // 
+            // txtFilmId
+            // 
+            this.txtFilmId.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilmId.Location = new System.Drawing.Point(203, 49);
+            this.txtFilmId.Name = "txtFilmId";
+            this.txtFilmId.Size = new System.Drawing.Size(180, 20);
+            this.txtFilmId.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Film No:";
+            // 
+            // gboPuan
+            // 
+            this.gboPuan.Controls.Add(this.rbPuanYok);
+            this.gboPuan.Controls.Add(this.rbPuan5);
+            this.gboPuan.Controls.Add(this.rbPuan4);
+            this.gboPuan.Controls.Add(this.rbPuan3);
+            this.gboPuan.Controls.Add(this.rbPuan2);
+            this.gboPuan.Controls.Add(this.rbPuan1);
+            this.gboPuan.Location = new System.Drawing.Point(9, 83);
+            this.gboPuan.Name = "gboPuan";
+            this.gboPuan.Size = new System.Drawing.Size(180, 207);
+            this.gboPuan.TabIndex = 4;
+            this.gboPuan.TabStop = false;
+            this.gboPuan.Text = "Puan";
+            // 
+            // rbPuan5
+            // 
+            this.rbPuan5.AutoSize = true;
+            this.rbPuan5.Location = new System.Drawing.Point(6, 164);
+            this.rbPuan5.Name = "rbPuan5";
+            this.rbPuan5.Size = new System.Drawing.Size(72, 17);
+            this.rbPuan5.TabIndex = 4;
+            this.rbPuan5.TabStop = true;
+            this.rbPuan5.Tag = "5";
+            this.rbPuan5.Text = "5 - Çok İyi";
+            this.rbPuan5.UseVisualStyleBackColor = true;
+            // 
+            // rbPuan4
+            // 
+            this.rbPuan4.AutoSize = true;
+            this.rbPuan4.Location = new System.Drawing.Point(6, 137);
+            this.rbPuan4.Name = "rbPuan4";
+            this.rbPuan4.Size = new System.Drawing.Size(50, 17);
+            this.rbPuan4.TabIndex = 3;
+            this.rbPuan4.TabStop = true;
+            this.rbPuan4.Tag = "4";
+            this.rbPuan4.Text = "4 - İyi";
+            this.rbPuan4.UseVisualStyleBackColor = true;
+            // 
+            // rbPuan3
+            // 
+            this.rbPuan3.AutoSize = true;
+            this.rbPuan3.Location = new System.Drawing.Point(6, 110);
+            this.rbPuan3.Name = "rbPuan3";
+            this.rbPuan3.Size = new System.Drawing.Size(60, 17);
+            this.rbPuan3.TabIndex = 2;
+            this.rbPuan3.TabStop = true;
+            this.rbPuan3.Tag = "3";
+            this.rbPuan3.Text = "3 - Orta";
+            this.rbPuan3.UseVisualStyleBackColor = true;
+            // 
+            // rbPuan2
+            // 
+            this.rbPuan2.AutoSize = true;
+            this.rbPuan2.Location = new System.Drawing.Point(6, 83);
+            this.rbPuan2.Name = "rbPuan2";
+            this.rbPuan2.Size = new System.Drawing.Size(62, 17);
+            this.rbPuan2.TabIndex = 1;
+            this.rbPuan2.TabStop = true;
+            this.rbPuan2.Tag = "2";
+            this.rbPuan2.Text = "2 - Kötü";
+            this.rbPuan2.UseVisualStyleBackColor = true;
+            // 
+            // rbPuan1
+            // 
+            this.rbPuan1.AutoSize = true;
+            this.rbPuan1.Location = new System.Drawing.Point(6, 56);
+            this.rbPuan1.Name = "rbPuan1";
+            this.rbPuan1.Size = new System.Drawing.Size(84, 17);
+            this.rbPuan1.TabIndex = 0;
+            this.rbPuan1.TabStop = true;
+            this.rbPuan1.Tag = "1";
+            this.rbPuan1.Text = "1 - Çok Kötü";
+            this.rbPuan1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Location = new System.Drawing.Point(218, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 34);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resmi Değiştirmek İçin Üzerine Tıklayınız";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtFilmAd
+            // 
+            this.txtFilmAd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilmAd.Location = new System.Drawing.Point(9, 49);
+            this.txtFilmAd.Name = "txtFilmAd";
+            this.txtFilmAd.Size = new System.Drawing.Size(180, 20);
+            this.txtFilmAd.TabIndex = 1;
+            this.txtFilmAd.TextChanged += new System.EventHandler(this.txtFilmAd_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Film Adı:";
+            // 
+            // pboFoto
+            // 
+            this.pboFoto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pboFoto.Location = new System.Drawing.Point(218, 90);
+            this.pboFoto.Name = "pboFoto";
+            this.pboFoto.Size = new System.Drawing.Size(150, 200);
+            this.pboFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pboFoto.TabIndex = 0;
+            this.pboFoto.TabStop = false;
+            // 
+            // lstFilmler
+            // 
+            this.lstFilmler.DisplayMember = "FilmAd";
+            this.lstFilmler.FormattingEnabled = true;
+            this.lstFilmler.Location = new System.Drawing.Point(405, 22);
+            this.lstFilmler.Name = "lstFilmler";
+            this.lstFilmler.Size = new System.Drawing.Size(287, 342);
+            this.lstFilmler.TabIndex = 0;
+            this.lstFilmler.ValueMember = "Id";
+            this.lstFilmler.SelectedIndexChanged += new System.EventHandler(this.lstFilmler_SelectedIndexChanged);
+            // 
+            // rbPuanYok
+            // 
+            this.rbPuanYok.AutoSize = true;
+            this.rbPuanYok.Location = new System.Drawing.Point(6, 29);
+            this.rbPuanYok.Name = "rbPuanYok";
+            this.rbPuanYok.Size = new System.Drawing.Size(93, 17);
+            this.rbPuanYok.TabIndex = 5;
+            this.rbPuanYok.TabStop = true;
+            this.rbPuanYok.Tag = "";
+            this.rbPuanYok.Text = "Puanlanmamış";
+            this.rbPuanYok.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(704, 377);
+            this.Controls.Add(this.lstFilmler);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Öz IMDB";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.gboPuan.ResumeLayout(false);
+            this.gboPuan.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboFoto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYeniFilm;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFilmId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox gboPuan;
+        private System.Windows.Forms.RadioButton rbPuan5;
+        private System.Windows.Forms.RadioButton rbPuan4;
+        private System.Windows.Forms.RadioButton rbPuan3;
+        private System.Windows.Forms.RadioButton rbPuan2;
+        private System.Windows.Forms.RadioButton rbPuan1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFilmAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pboFoto;
+        private System.Windows.Forms.ListBox lstFilmler;
+        private System.Windows.Forms.RadioButton rbPuanYok;
+    }
+}
+
---
+++ OzIMDB/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OzIMDB
+{
+    public partial class Form1 : Form
+    {
+        private string dbName = "OzImdb";
+        SqlConnection con = new SqlConnection("server=.;uid=sa; pwd=123;");
+        private List<Film> filmler;
+        private bool guncellemeAktif = false;
+
+        public Form1()
+        {
+            con.Open();
+            VeriTabaniYoksaOlustur();
+            InitializeComponent();
+            FilmleriListele();
+        }
+
+        private void FilmleriListele()
+        {
+            guncellemeAktif = false;
+            var cmd = new SqlCommand("Select * from Filmler", con);
+            var dr = cmd.ExecuteReader();
+
+            filmler = new List<Film>();
+
+            while (dr.Read())
+            {
+                filmler.Add(new Film()
+                {
+                    Id = (int)dr["Id"],
+                    FilmAd = (string)dr["FilmAD"],
+                    Puan = dr["Puan"] as int?,
+                    Foto = ConvertToImage(dr["Foto"] as byte[])
+                });
+            }
+            dr.Close();
+            lstFilmler.DataSource = filmler;
+            //pboFoto.Image = filmler[0].Foto;
+            guncellemeAktif = true;
+        }
+
+        private void VeriTabaniYoksaOlustur()
+        {
+            var cmd = new SqlCommand($"Select DB_ID('{dbName}')", con);
+            short? sonuc = cmd.ExecuteScalar() as short?;
+            if (!sonuc.HasValue) //sonuc==null
+            {
+                cmd = new SqlCommand($"CREATE DATABASE {dbName};", con);
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqlCommand($"USE {dbName};"
+                                     +@"CREATE TABLE Filmler
+(
+                                      Id INT PRIMARY KEY IDENTITY, 
+                                      FilmAd NVARCHAR(200) NOT NULL,
+                                      Puan INT NULL,
+                                      Foto VARBINARY(MAX) NULL
+);"
+                    , con);
+                cmd.ExecuteNonQuery();
+            }
+            else
+            {
+                cmd = new SqlCommand($"USE {dbName}", con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private Image ConvertToImage(byte[] data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+
+            return (Image) new ImageConverter().ConvertFrom(data);
+        }
+
+        private void btnYeniFilm_Click(object sender, EventArgs e)
+        {
+            int id = YeniFilmEkle();
+            FilmleriListele();
+
+            //foreach (var item in lstFilmler.Items)
+            //{
+            //    Film film = (Film)item;
+            //    if (film.Id == id)
+            //    {
+            //        lstFilmler.SelectedItem = item;
+            //        break;
+            //        ;
+            //    }
+            //}
+
+            lstFilmler.SelectedValue = id;
+
+        }
+
+        private int YeniFilmEkle()
+        {
+            var cmd = new SqlCommand("Insert into Filmler(FilmAd) Values(N'Yeni Film'); Select Scope_Identity();",con);
+            return Convert.ToInt32(cmd.ExecuteScalar());
+        }
+
+        private void lstFilmler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstFilmler.SelectedIndex == -1)
+            {
+                return;
+            }
+
+            guncellemeAktif = false;
+            Film film = (Film) lstFilmler.SelectedItem;
+            txtFilmId.Text = film.Id.ToString();
+            txtFilmAd.Text = film.FilmAd;
+
+            #region Puanı Goster
+            if (film.Puan == null)
+            {
+                rbPuanYok.Checked = true;
+            }
+            else
+            {
+                //#region Yontem 1
+                //RadioButton rb;
+                //foreach (Control control in gboPuan.Controls)
+                //{
+                //    if (control is RadioButton
+                //        && control.Tag != null
+                //        && control.Tag.ToString() == film.Puan.ToString())
+                //    {
+                //        ((RadioButton)control).Checked = true;
+                //        break;
+                //    }
+                //} 
+                //#endregion
+
+                #region Yontem 2
+                RadioButton[] rbDizi = { rbPuanYok, rbPuan1, rbPuan2, rbPuan3, rbPuan4, rbPuan5 };
+                rbDizi[film.Puan.Value].Checked = true;
+                #endregion
+            } 
+            #endregion
+
+            pboFoto.Image = film.Foto;
+            guncellemeAktif = true;
+
+        }
+
+        private void txtFilmAd_TextChanged(object sender, EventArgs e)
+        {
+            Guncelle();
+        }
+
+        private void Guncelle()
+        {
+            if (guncellemeAktif && lstFilmler.SelectedIndex > -1)
+            {
+                Film film = (Film) lstFilmler.SelectedItem;
+                film.FilmAd = txtFilmAd.Text;
+                // todo: puanı ve resmi güncelle
+
+                var cmd = new SqlCommand("Update filmler set FilmAd = @p1 where Id = @p4",con);
+                cmd.Parameters.AddWithValue("@p1", film.FilmAd);
+                cmd.Parameters.AddWithValue("@p4", film.Id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
